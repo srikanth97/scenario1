26cbc19deee5efd513bbdb11504b77a5e053c48e
srikanth97 <nksr.1997@gmail.com>
2019-10-28 11:41:23 +0530

    Scenario 1 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+      <h2>Enter text  you want to display in the console:</h2>
+      <input type="text" [(ngModel)]="display">
+      <button (click)="displayInConsole()">Display</button>
+  `
 })
 export class AppComponent {
   title = 'scenario1';
+  display: String;
+
+
+  displayInConsole() {
+    console.log(this.display);
+    this.display ="";
+  }
 }
